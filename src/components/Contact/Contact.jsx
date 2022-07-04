d5212d48da7bb69af248521c450b03fa1ead309b
AlexTeklu <alexanderosteklu@gmail.com>
2022-07-04 03:09:31 +0200

    updated contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,55 +1,64 @@
-import {MdOutlineAttachEmail,} from 'react-icons/md';
+import { MdOutlineAttachEmail } from 'react-icons/md';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+
 import './contact.css';
 import { useRef } from 'react';
 import emailjs from 'emailjs-com';
 
 const Contact = () => {
-   const form = useRef();
+	const form = useRef();
 
-   const sendEmail = (e) => {
-    e.preventDefault();
+	const sendEmail = (e) => {
+		e.preventDefault();
 
-    emailjs.sendForm('service_ll1lg2l', 'template_l3mnnpl', form.current, 'XjzG5bNYwaipJITXA')
-      
-    e.target.reset();
-  };
+		emailjs.sendForm(
+			'service_ll1lg2l',
+			'template_l3mnnpl',
+			form.current,
+			'XjzG5bNYwaipJITXA'
+		);
 
+		e.target.reset();
+	};
 
+	return (
+		<section id='contact'>
+			<h5>Get in Touch</h5>
+			<h2>Contact Me</h2>
+			<div className='container contact-conainer'>
+				<div className='contact-options'>
+					<article className='contact-option'>
+						<MdOutlineAttachEmail className='contact_option-icons' />
+						<h4>Email</h4>
+						<h5>alexander.teklu@hyperisland.se</h5>
+						<a href='mailto:alexanderosteklu@hyperisand.se' target={'/'}>
+							{' '}
+							Send a message
+						</a>
+					</article>
+					<article className='contact-option'>
+						<BsFillTelephoneFill className='contact_option-icons' />
+						<h4>Telephone</h4>
+						<h5>+46729428730</h5>
+					</article>
+				</div>
 
-  return (
-    <section id="contact">
-      <h5>Get in Touch</h5>
-      <h2>Contact Me</h2>
-      <div className="container contact-conainer">
-        <div className='contact-options'>
-          <article className='contact-option'>
-            <MdOutlineAttachEmail className='contact_option-icons'/>
-            <h4>Email</h4>
-            <h5>alexander.teklu@hyperisland.se</h5>
-            <a href="mailto:alexanderosteklu@hyperisand.se" target={'/'}> Send a message</a>
-          </article>
-          <article className='contact-option'>
-            <MdOutlineAttachEmail className='contact_option-icons'/>
-            <h4>email</h4>
-            <h5>alexanderosteklu@gmail.com</h5>
-            <a href="mailto: alexanderosteklu@gmail.com" target={'/'}> Send a message</a>
-          </article>
-        </div>
-
-
-        <form ref={form} onSubmit={sendEmail}>
-          <input type="text" 
-                 name='name'
-                 placeholder='Your Name' required/>
-                 <input type="email" 
-                 name='email'
-                 placeholder='Your Email' required/>
-                 <textarea name="message" rows="7" placeholder='your message' required></textarea>
-                 <button type='submit' className='btn btn-primary'>send message</button>
-        </form>
-      </div>
-    </section>
-  )
-}
+				<form ref={form} onSubmit={sendEmail}>
+					<input type='text' name='name' placeholder='Your Name' required />
+					<input type='email' name='email' placeholder='Your Email' required />
+					<textarea
+						name='message'
+						rows='7'
+						placeholder='your message'
+						required
+					></textarea>
+					<button type='submit' className='btn btn-primary'>
+						send message
+					</button>
+				</form>
+			</div>
+		</section>
+	);
+};
 
 export default Contact;
