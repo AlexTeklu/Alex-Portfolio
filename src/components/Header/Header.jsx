721c3d362d7fcb6e81b4d81a6333cd4ec45b8499
AlexTeklu <alexanderosteklu@gmail.com>
2022-08-31 14:11:33 +0200

    bug fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
-import myImage from '../.././Assets/mini1.png';
+import myImage from '../.././Assets/pic.jpg';
 import HeaderSocialM from './HeaderSocialM';
 
 const Header = () => {
